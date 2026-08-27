c6bab2f413b91f5e246b88c1e4349b21b507ed95
JoelMEsc <a01286527@tec.mx>
2026-08-27 09:23:51 -0600

    Add CRUD Agenda and Web-specific GHCR CI pipeline

--- web/main.py
+++ web/main.py
@@ -1,8 +1,48 @@
 import os
 from fastapi import FastAPI, HTTPException
+from prometheus_fastapi_instrumentator import Instrumentator
 from sqlalchemy import create_engine, text
+from pydantic import BaseModel
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
+
+agenda = {}
+current_id = 1
+
+class Contact(BaseMdeol):
+    name: str
+    phone: str
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Agenda API"}
+
+@app.post("/contacts/")
+def create_contact(contact: Contact):
+    global current_id
+    agenda[current_id] = contact.dict()
+    current_id += 1
+    return {"id": current_id - 1, **contact.dict()}
+
+@app.get("/contacts/")
+def read_contacts():
+    return {"count": len(agenda), "contacts": agenda}
+
+@app.put("/contacts/{contact_id}")
+def update_contact(contact_id: int, contact: Contact):
+    if contact_id not in agenda:
+        raise HTTPException(status_code=404, detail="Contact no found")
+    agenda[contact_id] = contact.dict()
+    return {"id": contact_id, **contact.dict()}
+
+@app.delete("/contacts/{contact_id}")
+def delete_contact(contact_id: int):
+    if contact_id not in agenda:
+        raise HTTPException(status_code=404, detail="Contact not found")
+    del agenda[contact_id]
+    return {"status": "deleted", "id": contact_id}
 
 @app.get("/")
 def root():
